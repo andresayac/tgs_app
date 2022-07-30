5c7feafb9a3535a39f1c8824706e7e5060fb4ac5
andresayac <andresayac@gmail.com>
2022-07-30 13:37:10 -0500

    first commit

---
+++ templates/element/Navs/sliderbar.php
@@ -0,0 +1,76 @@
+
+<?php
+?>
+
+<div class="left-side-bar">
+    <div class="brand-logo">
+        <a href="/">
+            <img src="vendors/images/deskapp-logo.svg" alt="" class="dark-logo">
+            <img src="vendors/images/deskapp-logo-white.svg" alt="" class="light-logo">
+        </a>
+        <div class="close-sidebar" data-toggle="left-sidebar-close">
+            <i class="ion-close-round"></i>
+        </div>
+    </div>
+    <div class="menu-block customscroll">
+        <div class="sidebar-menu">
+            <ul id="accordion-menu">
+                <li class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle">
+                        <span class="micon dw dw-house-1"></span><span class="mtext">Inicio</span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="/">Dashboard style 1</a></li>
+                    </ul>
+                </li>
+
+
+                <li class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle">
+                        <span class="micon dw dw-list3"></span><span class="mtext">Multi Level Menu</span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="javascript:;">Level 1</a></li>
+                        <li class="dropdown">
+                            <a href="javascript:;" class="dropdown-toggle">
+                                <span class="micon fa fa-plug"></span><span class="mtext">Level 2</span>
+                            </a>
+                            <ul class="submenu child">
+                                <li><a href="javascript:;">Level 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="javascript:;">Level 1</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/users" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-user"></span><span class="mtext">Usuarios</span>
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown-divider"></div>
+                </li>
+                <li>
+                    <div class="sidebar-small-cap">Extra</div>
+                </li>
+                <li>
+                    <a href="javascript:;" class="dropdown-toggle">
+                        <span class="micon dw dw-edit-2"></span><span class="mtext">Documentation</span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="#">Introduction</a></li>
+                        <li><a href="#">Getting Started</a></li>
+                        <li><a href="#">Color Settings</a></li>
+                        <li><a href="#">Third Party Plugins</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" target="_blank" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-paper-plane1"></span>
+                        <span class="mtext">Landing Page <img src="vendors/images/coming-soon.png" alt="" width="25"></span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ templates/layout/default.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @var \App\View\AppView $this
+ */
+
+$cakeDescription = 'CakePHP: the rapid development php framework';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $cakeDescription ?>:
+        <?= $this->fetch('title') ?>
+    </title>
+
+    <!-- General CSS Files -->
+    <?php echo $this->Html->css("/vendors/styles/core.css"); ?>
+    <?php echo $this->Html->css("/vendors/styles/icon-font.min.css"); ?>
+    <?php echo $this->Html->css("/src/plugins/datatables/css/dataTables.bootstrap4.min.css"); ?>
+    <?php echo $this->Html->css("/src/plugins/datatables/css/responsive.bootstrap4.min.css"); ?>
+    <?php echo $this->Html->css("/vendors/styles/style.css"); ?>
+
+
+    <!-- pace js -->
+    <?php echo $this->Html->script("/vendors/scripts/core.js"); ?>
+    <?php echo $this->Html->script("/vendors/scripts/script.min.js"); ?>
+    <?php echo $this->Html->script("/vendors/scripts/process.js"); ?>
+    <?php echo $this->Html->script("/vendors/scripts/layout-settings.js"); ?>
+    <?php echo $this->Html->script("/src/plugins/datatables/js/jquery.dataTables.min.js"); ?>
+    <?php echo $this->Html->script("/src/plugins/datatables/js/dataTables.bootstrap4.min.js"); ?>
+    <?php echo $this->Html->script("/src/plugins/datatables/js/dataTables.responsive.min.js"); ?>
+    <?php echo $this->Html->script("/src/plugins/datatables/js/responsive.bootstrap4.min.js"); ?>
+    <?php echo $this->Html->script("/vendors/scripts/dashboard.js"); ?>
+
+
+    <?= $this->Html->meta('icon') ?>
+
+
+    <?php
+    // $this->Html->css(['normalize.min', 'milligram.min', 'cake']) 
+
+
+    //$this->fetch('meta') 
+    //$this->fetch('css') 
+    //$this->fetch('script') 
+    ?>
+</head>
+
+<body>
+
+    <?php echo $this->element('Navs\navbar');  ?>
+    <?php echo $this->element('Navs\sliderbar');  ?>
+
+
+    <div class="mobile-menu-overlay"></div>
+
+    <div class="main-container">
+        <div class="pd-ltr-20">
+            <?= $this->Flash->render() ?>
+            <?= $this->fetch('content') ?>
+            <div class="row">
+                <div class="footer-wrap pd-20 mb-20 card-box">
+                    DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit Hingarajiya</a>
+                </div>
+            </div>
+        </div>
+
+        <footer>
+        </footer>
+
+</body>
+
+</html>
