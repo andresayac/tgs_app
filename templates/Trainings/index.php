5290dbcb7645a70578d569c84235a61b12288666
andresayac <andresayac@gmail.com>
2022-08-04 17:37:14 -0500

    Actualizo front en modulos

--- templates/Trainings/index.php
+++ templates/Trainings/index.php
@@ -1,54 +1,128 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Training[]|\Cake\Collection\CollectionInterface $trainings
  */
 ?>
-<div class="trainings index content">
-    <?= $this->Html->link(__('New Training'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Trainings') ?></h3>
-    <div class="table-responsive">
-        <table>
+
+
+<div class="page-header">
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <div class="title">
+                <h4>Capacitaciones</h4>
+            </div>
+            <nav aria-label="breadcrumb" role="navigation">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/trainings">Capacitaciones</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Lista</li>
+                </ol>
+            </nav>
+        </div>
+        <div class="col-md-6 col-sm-12 text-right">
+            <div class="dropdown">
+                <a class="btn btn-primary dropdown-toggle" href="/trainings/add">
+                    Nuevo Capacitación
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card-box mb-30">
+    <div class="pd-20">
+        <h4 class="text-blue h4">Lista de Capacitaciónes</h4>
+        <p class="mb-0"></p>
+    </div>
+    <div class="dataTables_wrapper dt-bootstrap4 no-footer">
+        <table id="datatable-trainings" class="data-table table stripe hover nowrap dataTable no-footer dtr-inline">
             <thead>
                 <tr>
                     <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('start_date') ?></th>
-                    <th><?= $this->Paginator->sort('end_date') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('designations') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
+                    <th><?= $this->Paginator->sort('name', 'Nombre') ?></th>
+                    <th><?= $this->Paginator->sort('designations','Cargos') ?></th>
+                    <th><?= $this->Paginator->sort('start_date', 'Fecha Inicio') ?></th>
+                    <th><?= $this->Paginator->sort('end_date', 'Fecha Fin') ?></th>
+                    <th class="actions"><?= __('Acciones') ?></th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($trainings as $training): ?>
-                <tr>
-                    <td><?= $this->Number->format($training->id) ?></td>
-                    <td><?= h($training->start_date) ?></td>
-                    <td><?= h($training->end_date) ?></td>
-                    <td><?= h($training->name) ?></td>
-                    <td><?= h($training->designations) ?></td>
-                    <td><?= h($training->created) ?></td>
-                    <td><?= h($training->modified) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $training->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $training->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $training->id], ['confirm' => __('Are you sure you want to delete # {0}?', $training->id)]) ?>
-                    </td>
-                </tr>
+                <?php foreach ($trainings as $training) : ?>
+                    <tr>
+                        <td><?= $this->Number->format($training->id) ?></td>
+                        <td><?= h($training->name) ?></td>
+                        <td><?= h($training->designations) ?></td>
+                        <td><?= h($training->start_date) ?></td>
+                        <td><?= h($training->end_date) ?></td>
+                        <td>
+                            <div class="dropdown">
+                                <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                                    <i class="dw dw-more"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                    <?= $this->Html->link(__('Ver'), ['action' => 'view', $training->id], [
+                                        'escape' => false,
+                                        'class' => 'dropdown-item'
+                                    ]) ?>
+                                    <?= $this->Html->link(__('Editar'), ['action' => 'edit', $training->id], [
+                                        'escape' => false,
+                                        'class' => 'dropdown-item'
+                                    ]) ?>
+                                    <?= $this->Form->postLink(__('Eliminar'), ['action' => 'delete', $training->id], ['class' => 'dropdown-item', 'confirm' => __('Esta seguro que quiere eliminar la capacitación # {0}?', $training->id)]) ?>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
     </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
+
 </div>
+<script>
+    $(document).ready(function() {
+
+        $('.data-table').DataTable({
+            scrollCollapse: true,
+            autoWidth: false,
+            responsive: true,
+            columnDefs: [{
+                targets: "datatable-nosort",
+                orderable: false,
+            }],
+            "lengthMenu": [
+                [10, 25, 50, -1],
+                [10, 25, 50, "All"]
+            ],
+            "language": {
+                "info": "_START_-_END_ de _TOTAL_ registros",
+                searchPlaceholder: "Buscar",
+                paginate: {
+                    next: '<i class="ion-chevron-right"></i>',
+                    previous: '<i class="ion-chevron-left"></i>'
+                },
+                "sProcessing": "Procesando...",
+                "sLengthMenu": "Mostrar _MENU_ registros",
+                "sZeroRecords": "No se encontraron resultados",
+                "sEmptyTable": "Ningún dato disponible en esta tabla",
+                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sSearch": "Buscar:",
+                "sUrl": "",
+                "sInfoThousands": ",",
+                "sLoadingRecords": "Cargando...",
+                "oAria": {
+                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                }
+            },
+        });
+    });
+</script>
+
+
+
+
+
