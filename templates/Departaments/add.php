a6d9f60db13facb12d1e61526333181ab990e919
andresayac <andresayac@gmail.com>
2022-08-05 23:06:18 -0500

    Cambios en Areas

--- templates/Departaments/add.php
+++ templates/Departaments/add.php
@@ -4,25 +4,58 @@
  * @var \App\Model\Entity\Departament $departament
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Departaments'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="departaments form content">
-            <?= $this->Form->create($departament) ?>
-            <fieldset>
-                <legend><?= __('Add Departament') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('active');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+
+<div class="page-header">
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <div class="title">
+                <h4>Nueva Area</h4>
+            </div>
+            <nav aria-label="breadcrumb" role="navigation">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/departaments">Areas</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Nueva</li>
+                </ol>
+            </nav>
         </div>
     </div>
+</div>
+
+<div class="card-box mb-30">
+    <div class="pd-20">
+        <h4 class="text-blue h4">Datos del Area</h4>
+        <p class="mb-0"></p>
+    </div>
+    <?= $this->Form->create($departament) ?>
+    <div class="pd-20 card-box mb-30">
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                        <div class="form-group">
+                            <?= $this->Form->control('name', ['label' => 'Nombre de la Sucursal', 'class' => 'form-control']); ?>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <label>Activa</label>
+                        <?= $this->Form->select('active', ['0' => 'NO', '1' => 'SI'], ['empty' => false, 'class' => 'selectpicker form-control', 'label' => false, 'required' => true]) ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                        <div class="form-group">
+                            <label>Notas</label>
+                            <?= $this->Form->input('note', ['class' => 'form-control', 'type' => 'textarea']) ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 col-sm-12 pd-20 mb-15">
+                    <div class="form-group">
+                        <?= $this->Form->button('Crear Area', ['class' => 'btn btn-outline-primary']) ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?= $this->Form->end() ?>
 </div>
--- templates/Departaments/view.php
+++ templates/Departaments/view.php
@@ -1,44 +1,56 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Departament $departament
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Departament'), ['action' => 'edit', $departament->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Departament'), ['action' => 'delete', $departament->id], ['confirm' => __('Are you sure you want to delete # {0}?', $departament->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Departaments'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Departament'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+
+<div class="page-header">
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <div class="title">
+                <h4>Areas</h4>
+            </div>
+            <nav aria-label="breadcrumb" role="navigation">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/departaments">Area</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Ver</li>
+                </ol>
+            </nav>
         </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="departaments view content">
-            <h3><?= h($departament->name) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Name') ?></th>
-                    <td><?= h($departament->name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($departament->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Active') ?></th>
-                    <td><?= $departament->active === null ? '' : $this->Number->format($departament->active) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
-                    <td><?= h($departament->created) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Modified') ?></th>
-                    <td><?= h($departament->modified) ?></td>
-                </tr>
-            </table>
+    </div>
+</div>
+
+<div class="card-box mb-30">
+    <div class="pd-20">
+        <h4 class="text-blue h4">Datos del Area</h4>
+        <p class="mb-0"></p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-30">
+        <div class="pd-20 card-box height-100-p">
+
+            <h5 class="text-center h5 mb-0"></h5>
+            <p class="text-center text-muted font-14">
+                <strong><?= h($departament->name) ?></strong>
+            </p>
+            <div class="profile-info">
+                <h5 class="mb-20 h5 text-blue">Información del Area</h5>
+                <ul>
+                    <li>
+                        <span>Area:</span>
+                        <?= h($departament->name) ?>
+                    </li>
+                    <li>
+                        <span>Información Complementaria:</span>
+                        Completar a Futuro
+                    </li>
+                </ul>
+            </div>
+
         </div>
     </div>
 </div>
