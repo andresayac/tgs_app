415ce0add15caf0a401b6684aabd557f85bc0294
andresayac <andresayac@gmail.com>
2022-08-09 21:22:20 -0500

    fix

--- templates/Trainings/attendance.php
+++ templates/Trainings/attendance.php
@@ -366,7 +366,7 @@ $training->set('end_hour', $training->end_date->format('H:i'));
                 "sLengthMenu": "Mostrar _MENU_ registros",
                 "sZeroRecords": "No se encontraron resultados",
                 "sEmptyTable": "Ningún dato disponible en esta tabla",
-                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "sInfoEmpty": "Mostrando registros del 0 al 0 <br> de un total de 0 registros",
                 "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
                 "sInfoPostFix": "",
                 "sSearch": "Buscar:",
@@ -374,8 +374,8 @@ $training->set('end_hour', $training->end_date->format('H:i'));
                 "sInfoThousands": ",",
                 "sLoadingRecords": "Cargando...",
                 "oAria": {
-                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                    "sSortAscending": ": Activar para ordenar la columna <br> de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna <br> de manera descendente"
                 }
             },
         });
