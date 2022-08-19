1bcf120ba85214842a1b26a17ef46674dc4ca1da
andresayac <andresayac@gmail.com>
2022-08-18 21:06:20 -0500

    change

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -56,6 +56,7 @@ $cakeDescription = 'TGS';
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"> 
     <meta name="theme-color" content="#343a40">
 
 </head>
