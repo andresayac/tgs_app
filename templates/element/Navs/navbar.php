ca7187179905357e6ff3ff092f106686e7f2772c
andresayac <andresayac@gmail.com>
2022-08-30 12:55:56 -0500

    Cambio de icono

--- templates/element/Navs/navbar.php
+++ templates/element/Navs/navbar.php
@@ -13,10 +13,10 @@
             </div>
         </div>
         <div class="user-info-dropdown">
-            <div class="dropdown">
-                <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
-                    <span class="user-icon-app">
-                        <i class="icon-copy dw dw-user"></i>
+            <div class="dropdown show">
+                <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="true">
+                    <span class="user-icon">
+                        <img src="vendors/images/profile.png" alt="">
                     </span>
                     <span class="user-name" style="vertical-align: unset;"><?= explode(' ', $_logged_user_['fullname'])[0] ?? ''  ?></span>
                 </a>
